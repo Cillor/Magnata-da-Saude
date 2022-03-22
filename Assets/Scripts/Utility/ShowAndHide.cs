c7adfa9cce86aba97563cef969a0c88171751b9f
Matheus Cirillo <matheuscirillo@usp.br>
2022-03-22 10:27:41 -0300

    git index reset

--- Assets/Scripts/Utility/ShowAndHide.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ShowAndHide : MonoBehaviour{
-    public void ToggleGameObject(){
-        gameObject.SetActive(!gameObject.activeSelf);
-    }
-}
