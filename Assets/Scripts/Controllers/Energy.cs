81a01e7b5ae285cf782fb90d6eb58cba5a383b2d
Matheus Cirillo <matheuscirillo@usp.br>
2022-04-04 22:38:10 -0300

    Character death

--- Assets/Scripts/Controllers/Energy.cs
+++ Assets/Scripts/Controllers/Energy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Energy : MonoBehaviour{
@@ -10,19 +11,40 @@ public class Energy : MonoBehaviour{
     private void Start() {
         state = SaveManager.Instance.state;
         energySlider.value = state.energy;
+
+        Clock.OnHourChange += ExistenceEnergyExpenditure;
+    }
+
+    void ExistenceEnergyExpenditure(){
+        ChangeEnergy(-0.001f);
     }
 
     public void ChangeEnergy(float change){
         //?Debug.Log("Energy expent: " + change);
-        state.energy += change;
+        float energyDelta;
+        if(change >= 0){
+            energyDelta = change * (68 - state.hoursSinceLastSlept)/52;
+            energyDelta *= (2100 + state.dailyCalorieRealDefficit)/2100;
+        }else{
+            energyDelta = change * (88 + state.hoursSinceLastSlept)/104;
+            energyDelta *= (2100 - state.dailyCalorieRealDefficit)/2100;
+        }
+        Debug.Log("Energy delta: " + energyDelta);
+
+        state.energy += energyDelta;
         state.energy = Mathf.Clamp(state.energy, 0, 1);
         energySlider.value = state.energy;
 
-        if(change < 0){
+        if(energyDelta < 0){
             FindObjectOfType<Indicators>().AddMessage("Energia ↓", Color.red);
         }else{
             FindObjectOfType<Indicators>().AddMessage("Energia ↑", Color.green);
         }
+
+        if(state.energy <= 0){
+            SceneManager.LoadScene("EnergyDeath");
+        }
+
         //change graph
     }
 }
--- Assets/Scripts/Controllers/Sleep.cs
+++ Assets/Scripts/Controllers/Sleep.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class Sleep : MonoBehaviour
@@ -19,6 +20,16 @@ public class Sleep : MonoBehaviour
         state = SaveManager.Instance.state;
         amountOfHoursToSleepText.text = TimeSpan.FromHours(1).ToString("hh'hrs'mm'min'");
         wakeUpHourAndMinuteText.text = clock.date.AddHours(1).ToString("HH:mm");
+        Clock.OnDayChange += NewDay;
+        Clock.OnHourChange += HourlyUpdate;
+    }
+
+    void NewDay(){
+        state.numberOfSleeps++;
+    }
+
+    void HourlyUpdate(){
+        state.hoursSinceLastSlept++;
     }
 
     public void ChangeAmountOfSleep(float value){
@@ -39,7 +50,6 @@ public class Sleep : MonoBehaviour
         sleepSelectionScreen.SetActive(false);
 
         state.totalHoursSlept += amountOfSleepWantedInHours;
-        state.numberOfSleeps++;
 
         float sleepAverage = state.totalHoursSlept/state.numberOfSleeps;
         state.sleepQuality = -Mathf.Pow(0.4f * (sleepAverage - 8), 2) + 1;
@@ -53,6 +63,15 @@ public class Sleep : MonoBehaviour
         float energyIncreaseValue = UnityEngine.Random.Range(0.02f, 0.05f);
         energyIncreaseValue *= amountOfSleepWantedInHours * sleepQuality;
         energy.ChangeEnergy(energyIncreaseValue);
+
+        //changes Sleep deficit
+        state.hoursSinceLastSlept -= (amountOfSleepWantedInHours)*2.2f;
+        
+        if(state.hoursSinceLastSlept <= 0)
+            state.hoursSinceLastSlept = 0;
+        
+        if(state.hoursSinceLastSlept > (24*5))
+            SceneManager.LoadScene("SleepDeath");
     }
 
     //TODO Hours without sleeping
